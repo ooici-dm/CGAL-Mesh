b578e81a1c233da57688389f6583b591da04f6e0
Swarbhanu Chatterjee <SChatterjee@asascience.com>
2012-05-14 16:42:48 -0400

    Added method to iterate over the vertices of facets

--- examples/Polyhedron/polyhedron_prog_incr_builder.cpp
+++ examples/Polyhedron/polyhedron_prog_incr_builder.cpp
@@ -1,8 +1,27 @@
+/*
+ * Author: Swarbhanu Chatterjee
+ *
+ * Description: This is code allows us to build a triangle or a quadrilateral incrementally on
+ * a polyhedron. We are exploring the possibility that a polyhedron can be used as a mesh. In order
+ * to investigate the above we have a method in this module which runs a vertex iterator, an edge iterator,
+ * and a facet iterator over the polyhedron. The output is neatly printed and allows one to study what
+ * has been created.
+ *
+ */
+
+
+
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Polyhedron_incremental_builder_3.h>
 #include <CGAL/Polyhedron_3.h>
 #include <iostream>
 #include <CGAL/IO/Polyhedron_iostream.h>
+
+
+typedef CGAL::Simple_cartesian<double>     Kernel;
+typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
+typedef Polyhedron::HalfedgeDS             HalfedgeDS;
+
 
 // A modifier creating a triangle with the incremental builder.
 template <class HDS>
@@ -77,10 +96,6 @@ public:
 };
 
 
-typedef CGAL::Simple_cartesian<double>     Kernel;
-typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
-typedef Polyhedron::HalfedgeDS             HalfedgeDS;
-
 // a helper method for running different iterators
 void running_iterators( Polyhedron& P) {
     if ( P.size_of_facets() == 0)
@@ -104,7 +119,7 @@ void running_iterators( Polyhedron& P) {
     -- last_f;  // the last of the old facets
 
     int k = 0;
-    Polyhedron::Facet_iterator f = P.facets_begin();    // create new center vertices
+    Polyhedron::Facet_iterator f = P.facets_begin();
 
     do {
     	std::cout << "Printing a facet index: " << k++ <<  std::endl;
@@ -178,6 +193,30 @@ void running_iterators( Polyhedron& P) {
     CGAL_postcondition( P.is_valid());
 }
 
+void running_vertex_iterator_over_triangle( Polyhedron& P) {
+
+    int k = 0;
+
+	for (Polyhedron::Facet_iterator f = P.facets_begin(); f != P.facets_end(); ++f) {
+
+		std::cout << "Iterating over the facet with index " << k++ <<std::endl;
+		if ( P.size_of_facets() == 1)
+				std::cout << "The polyhedron has only one facet!" << std::endl;
+		std::cout << std::endl;
+
+		int n=0;
+		 for (Polyhedron::Vertex_iterator vi = P.vertices_begin(); vi != P.vertices_end(); ++vi)
+		 {
+			 Kernel::Point_3 p;
+			 p = vi->point();
+			 std::cout << "Coordinates of vertex " << n++ << std::endl;
+			 std::cout << "p.x() = "  << p.x() << std::endl;
+			 std::cout << "p.y() = "  << p.y() << std::endl;
+			 std::cout << "p.z() = "  << p.z() << std::endl;
+		 }
+	 }
+}
+
 
 int main() {
     Polyhedron P;
@@ -189,11 +228,9 @@ int main() {
     
     std::cout << "The polyhedron created at this stage" << P << std::endl;
     
-    running_iterators(P);
+//    running_iterators(P);
 
-    // We use std::transform to put double and vectors on the iterators
-
-
+    running_vertex_iterator_over_triangle(P);
 
     return 0;
 }
